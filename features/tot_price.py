8c42feebaa5fae289bfea5443b98473118a77a4f
tommaso <tommaso.fiora8@gmail.com>
2020-05-27 11:45:30 +0200

    Fix Feature

--- features/tot_price.py
+++ features/tot_price.py
@@ -1,21 +1,26 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-def impute_last_salesW1(df_train,sku_list):
-    #tot_price_wk1 = []
+def impute_last_salesW1(df,sku_list):
 
     output = []
+
     for i in sku_list:
-        salesW1 = np.asarray(df_train[df_train.sku == i]['sales w-1'])
+        salesW1 = np.asarray(df[df.sku == i]['sales w-1'])
         sales1 = []
+        # per ogni sku si prendono i salesW1 tranne il primo valore (che è Nan)
         for j in range(1, len(salesW1)):
             sales1.append(salesW1[j])
         sales1 = np.asarray(sales1).reshape(-1, 1)
-        price = np.asarray(df_train[df_train.sku == i].price)
 
-        price1 = (price[0:132]).reshape(-1, 1)
-        target_X = price[132].reshape(-1, 1)
-        # print(len(price),len(sales1))
+        price = np.asarray(df[df.sku == i].price)
+
+        price1 = (price[0:(price.shape[0] - 1)]).reshape(-1, 1)
+        target_X = price[(price.shape[0] - 1)].reshape(-1, 1)
+        # print(len(price1),len(sales1))
+
+        # si usa una Linear regression con valori di input price1 e target sales1 (per il train) per
+        # imputare l'ultimo valore di salesW1 per ogni sku
         model = LinearRegression().fit(price1, sales1)
         # print(target_X)
         out = model.predict(target_X)
@@ -23,30 +28,30 @@ def impute_last_salesW1(df_train,sku_list):
     return output
 
 
-def tot_price_per_wk(df_train):
-    #tot_price_wk1=[]
+def tot_price_per_wk(df):
 
-    sku_list = list(set(df_train.sku))
+    sku_list = list(set(df.sku))
     sku_list.sort()
-    output = impute_last_salesW1(df_train, sku_list)
+    output = impute_last_salesW1(df, sku_list)
     tot_sales = []
     n_price = []
     for i, sku in enumerate(sku_list):
-        # print(i,sku_list)
-        n_price.append(df_train[df_train.sku == sku].price)
-        n_sales = np.asarray(df_train[df_train.sku == sku]['sales w-1'])[1:133]
-        # print(output[i][0][0])
+
+        n_price.append(df[df.sku == sku].price)
+        n_sales = np.asarray(df[df.sku == sku]['sales w-1'])
+        n_sales = n_sales[1:(n_sales.shape[0])]
         tot_sales.append(np.append(n_sales, output[i][0][0]))
-        # print(tot_sales)
+
+    # tot_sales e n_price adesso hanno la stessa lunghezza e corrispondono alla stessa settimana
     tot_sales = np.asarray(tot_sales)
 
     n_price = np.asarray(n_price)
-    # print(tot_sales,n_price)
-    tot_price_wk = tot_sales * n_price
-    tot_price_new = []
-    for i in tot_price_wk:
-        for j in range(len(i)):
-            tot_price_new.append(i[j])
 
-    df_train['tot_cost'] = tot_price_new
-    return df_train
+    tot_price_wk = tot_sales * n_price
+
+    #print(np.concatenate(tot_price_wk, axis=0))
+    tot_price_wk = np.concatenate(tot_price_wk, axis=0)
+
+
+    df['tot_cost'] = tot_price_wk
+    return df
