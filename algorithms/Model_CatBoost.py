cf4807d1c9d68bf8fdcf45657ed035b7e1c27104
teomores <matmoresc@gmail.com>
2020-05-29 16:26:47 +0200

    schifo ma fa 8.7 catboost

---
+++ algorithms/Model_CatBoost.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from sklearn.preprocessing import LabelEncoder
+from catboost import CatBoostRegressor
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.append('.')
+
+s = None        # Global variable to define the number of different skus in the train, used in the custom objective function
+
+class CatBoost(object):
+
+    def __init__(self, train, test, categorical_features, drop_columns, name='', isScope=True, sample_weights=None, evaluation=False):
+        global s
+
+        if isScope:
+            test = test[test.scope==1]
+
+        self.drop_columns = drop_columns
+
+        self.X_train = train.drop(['target'] + drop_columns, axis=1)
+        self.y_train = train.target
+
+        self.X_test = test.copy()
+        #self.X_test = test.drop(['target'] + drop_columns, axis=1)
+        self.y_test = test.target
+
+        self.cat_features = categorical_features
+        self.params = {
+                       # 'metric': 'huber',   # Se si cambia la metrica non si cambia l'ottimizzazione
+                    #    'objective': CatBoost.wmape_train_,  # Per ottimizzare con una particolare metrica dobbiamo usare l'objective
+                       'iterations': 100,
+                        'learning_rate': 0.2,
+                        'depth': 4,
+                        'verbose': False
+                       }
+
+        self.model = CatBoostRegressor(**self.params)
+
+        self.name = name
+
+        self.evaluation = evaluation
+
+        s = len(set(self.X_train.sku.values))
+
+    def fit(self,):
+        if self.evaluation:
+            self.model.fit(self.X_train, self.y_train, cat_features=self.cat_features)
+        else:
+            self.model.fit(self.X_train, self.y_train, cat_features=self.cat_features)
+
+
+    def predict(self,):
+        self.X_test['log_prediction' + self.name] = self.model.predict(self.X_test.drop(['target'] + self.drop_columns, axis=1))
+        self.X_test['prediction' + self.name] = np.expm1(self.X_test['log_prediction' + self.name])
+
+        return self.X_test[['Date', 'sku', 'target', 'real_target', 'log_prediction' + self.name, 'prediction' + self.name]]
+
+    def run(self):
+        if self.evaluation:
+            self.fit()
+        else:
+            self.fit()
+            return self.predict()
+
+    def get_model(self):
+        return self.model
+
+    def plot_feature_importance(self, plot_title):
+        plt.figure(figsize=(8, 5))
+        plt.xticks(rotation=90)
+        plt.title(plot_title)
+        plt.plot(self.X_train.columns, self.model.get_feature_importance)
+        plt.show()
---
+++ cat.py
@@ -0,0 +1,153 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from sklearn.preprocessing import LabelEncoder
+from algorithms.Model_CatBoost import CatBoost
+
+import sys
+#sys.path.append('.')
+
+from preprocessing.preprocessing import convert_date, inverse_interpolation, train_validation_split
+from metrics.MAPE import MAPE
+
+from utils import get_weights, dfs_gen, add_all_features
+
+train = pd.read_csv("dataset/original/train.csv")
+test = pd.read_csv("dataset/original/x_test.csv")
+
+useTest = True
+useScope = True
+isEvaluation = False
+useSampleWeights, weights_type = True, 0
+
+if isEvaluation:
+    useTest = False
+    useScope = False
+#   --------------- Preprocessing -----------------
+
+df = pd.concat([train, test])
+df = convert_date(df)
+
+if useTest:
+    df = df.sort_values('Date')
+    df_dates = df[df.target.isna()]
+    df_dates = df_dates.drop_duplicates('Date').Date
+
+    # Riga da RIMUOVERE PRIMA DELLA CONSEGNA
+    df.loc[df.target.isna(),'target'] = df[df.target.isna()][['Date', 'sku','sales w-1']].groupby('sku')['sales w-1'].shift(-1).values
+
+
+df = df.sort_values(['sku','Date']).reset_index(drop=True)
+
+# Encoding Categorical Features
+le = LabelEncoder()
+df.pack = le.fit_transform(df.pack)
+df.brand = le.fit_transform(df.brand)
+
+# Impute sales w-1 NaNs in the first week
+df = inverse_interpolation(df)
+
+#   --------------- Features -----------------
+
+"""The log function essentially de-emphasizes very large values.
+It is more easier for the model to predict correctly if the distribution is not that right-skewed which is
+corrected by modelling log(sales) than sales."""
+
+# real_values = df[['Date', 'sku', 'target']].rename(columns={'target':'real_target'})
+df['real_target'] = df.target
+df['target'] = np.log1p(df.target.values)
+df['sales w-1'] = np.log1p(df['sales w-1'].values)
+
+df, categorical_f = add_all_features(df)
+categorical_f = ['sku', 'pack', 'brand'] + categorical_f
+
+#   --------------- Split -----------------
+df = df.sort_values('Date')
+
+# Train-Test split
+train = df[~df.target.isna()]
+test = df[df.target.isna()]
+
+# Train-Validation split
+_, _, val_dates = train_validation_split(train)
+
+
+if useTest:
+    gen = dfs_gen(df, df_dates)
+else:
+    gen = dfs_gen(train, val_dates)
+
+#   --------------- Model -----------------
+
+drop_cols = ['scope', 'Date', 'real_target','pack', 'size (GM)']
+categorical_f = [x for x in categorical_f if x not in drop_cols]
+
+prediction_df = pd.DataFrame()
+pred_cluster = pd.DataFrame()
+
+sample_weights = None
+
+for df_train, df_test in tqdm(gen):
+
+    if useSampleWeights:
+        sample_weights = get_weights(df_train, type=weights_type)
+
+    model = CatBoost(df_train, df_test, categorical_features=categorical_f, drop_columns=drop_cols, isScope=useScope,
+                     sample_weights=sample_weights, evaluation=isEvaluation)
+    model_preds = model.run()
+
+    prediction_df = pd.concat([prediction_df, model_preds])
+
+
+    # ---- Predict by cluster  -----
+
+    #  -----------   Cluster 1
+    drop_cols = drop_cols + ['cluster']
+    categorical_f = [x for x in categorical_f if x not in drop_cols]
+
+    cluster_model_1 = CatBoost(df_train[df_train.cluster == 1], df_test[df_test.cluster == 1],
+                               categorical_features=categorical_f, drop_columns=drop_cols, name='_c', isScope=useScope,)
+    cluster_pred_1 = cluster_model_1.run()
+
+    pred_cluster = pd.concat([pred_cluster, cluster_pred_1])
+
+    #  -----------   Cluster 2
+    cluster_model_2 = CatBoost(df_train[df_train.cluster == 2], df_test[df_test.cluster == 2],
+                               categorical_features=categorical_f, drop_columns=drop_cols, name='_c', isScope=useScope,)
+    cluster_pred_2 = cluster_model_2.run()
+
+    pred_cluster = pd.concat([pred_cluster, cluster_pred_2])
+
+    if not useScope:
+        #  -----------   Cluster 3
+        cluster_model_3 = CatBoost(df_train[df_train.cluster == 3], df_test[df_test.cluster == 3],
+                                   categorical_features=categorical_f, drop_columns=drop_cols, name='_c',
+                                   isScope=useScope,)
+        cluster_pred_3 = cluster_model_3.run()
+
+        pred_cluster = pd.concat([pred_cluster, cluster_pred_3])
+
+
+if not isEvaluation:
+    # Merge Cluster Predictions with Standard LightGBM Predictions
+    prediction_df = prediction_df.merge(pred_cluster, how='left', on=['Date', 'sku', 'target', 'real_target'])
+
+    # Weighted Mean log_predictions
+    res = []
+    for l, l_c in tqdm(zip(prediction_df.log_prediction, prediction_df.log_prediction_c)):
+        final_pred = (0.4 * l + 0.6 * l_c)
+        res.append(final_pred)
+
+    prediction_df['final_pred'] = np.expm1(res)
+
+
+    if useTest:
+        print(f'Standard MAPE = {MAPE(prediction_df[prediction_df.Date.isin(df_dates[:-1])].real_target,prediction_df[prediction_df.Date.isin(df_dates[:-1])].prediction)}')
+        print(f'Cluster MAPE = {MAPE(prediction_df[prediction_df.Date.isin(df_dates[:-1])].real_target,prediction_df[prediction_df.Date.isin(df_dates[:-1])].prediction_c)}')
+        print(f'Ensemble MAPE = {MAPE(prediction_df[prediction_df.Date.isin(df_dates[:-1])].real_target,prediction_df[prediction_df.Date.isin(df_dates[:-1])].final_pred)}')
+
+
+    else:
+        print(f'Standard MAPE = {MAPE(prediction_df.real_target, prediction_df.prediction)}')
+        print(f'Cluster MAPE = {MAPE(prediction_df.real_target, prediction_df.prediction_c)}')
+        print(f'Ensemble MAPE = {MAPE(prediction_df.real_target, prediction_df.final_pred)}')
